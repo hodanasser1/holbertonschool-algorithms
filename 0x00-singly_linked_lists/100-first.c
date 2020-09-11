52feadb0b7340271e656b69e140bf4d525b355c0
hodanasser1 <hoda.nasser933@gmail.com>
2020-09-11 20:54:46 +0000

    test

--- 0x00-singly_linked_lists/100-first.c
+++ 0x00-singly_linked_lists/100-first.c
@@ -1,11 +1,10 @@
 #include "lists.h"
 
 /**
- * priority - function that prints before main executes
+ * priority - function that prints before main execute
  * Return: void
  */
 void __attribute__((constructor)) priority()
 {
-	printf("You're beat! and yet, you must allow,\n");
-	printf("I bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n""I bore my house upon my back!\n");
 }

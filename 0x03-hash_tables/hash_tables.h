b1b6056105a9b018c17b849694e82f52dc436d80
hoda <61836080+hodanasser1@users.noreply.github.com>
2020-09-20 02:01:27 +0300

    Update hash_tables.h

--- 0x03-hash_tables/hash_tables.h
+++ 0x03-hash_tables/hash_tables.h
@@ -17,7 +17,8 @@ typedef struct hash_node_s
         char *key;
         char *value;
         struct hash_node_s *next;
-} hash_node_t;
+}
+hash_node_t;
 
 /**
  * struct hash_table_s - Hash table data structure
@@ -31,7 +32,8 @@ typedef struct hash_table_s
 {
         unsigned long int size;
         hash_node_t **array;
-} hash_table_t;
+}
+hash_table_t;
 
 hash_table_t *hash_table_create(unsigned long int size);
 unsigned long int hash_djb2(const unsigned char *str);

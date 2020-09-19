8b3c7698a407924ef342c1499b56ee27f9030775
hodanasser1 <hoda.nasser933@gmail.com>
2020-09-19 23:21:20 +0000

    first commit

--- 0x03-hash_tables/4-hash_table_get.c
+++ 0x03-hash_tables/4-hash_table_get.c
@@ -9,18 +9,18 @@
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	unsigned long int i = 0;
-	hash_node_t *hn;
+	hash_node_t *temp;
 
 	if (ht == NULL || key == NULL)
 		return (NULL);
 
 	i = key_index((const unsigned char *)key, ht->size);
-	hn = ht->array[i];
+	temp = ht->array[i];
 
-	for (; hn != NULL; hn = hn->next)
+	for (; temp != NULL; temp = temp->next)
 	{
-		if (strcmp(hn->key, key) == 0)
-			return (hn->value);
+		if (strcmp(temp->key, key) == 0)
+			return (temp->value);
 	}
 	return (NULL);
 }
